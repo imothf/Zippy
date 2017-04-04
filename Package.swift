6ac4c108ba03369b256253531587e438ad41a7be
Dmitry Dovgoshliubnyi <darthdimon@googlemail.com>
2017-04-04 12:17:15 +0100

    Rearranged

--- Package.swift
+++ Package.swift
@@ -2,10 +2,6 @@ import PackageDescription
 
 let package = Package(
     name: "Zippy",
-    targets: [
-              Target(name: "ZipArchive", dependencies:[])
-        
-    ],
     dependencies: [
         .Package(url: "https://github.com/imothf/minizip", majorVersion: 1, minor: 0)
     ]
